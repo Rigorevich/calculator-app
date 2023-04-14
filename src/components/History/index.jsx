2015f4bbf35e22befcf6442473615f9c1e755b32
Rigorevich Ilya <rigorevich268@gmail.com>
2023-04-14 13:22:41 +0300

    add History

---
+++ src/components/History/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HBox, HTitle } from "./styled";
+
+const History = () => {
+  return (
+    <HBox>
+      <HTitle>History</HTitle>
+    </HBox>
+  );
+};
+
+export default History;
---
+++ src/components/History/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HBox = styled.div``;
+
+export const HTitle = styled.h2`
+  text-align: center;
+  font-weight: 500;
+  font-size: 26px;
+  color: ${(props) => props.theme.text};
+`;
