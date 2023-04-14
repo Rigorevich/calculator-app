e77a42a6a6bbef235f44a3d723397dc322225e2f
Rigorevich Ilya <rigorevich268@gmail.com>
2023-04-14 13:23:45 +0300

    combined all components in Calculator

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -1,68 +1,25 @@
 import React from "react";
-import {
-  Container,
-  CalculatorBlock,
-  HistoryBlock,
-  DisplayBox,
-  DisplayInput,
-  KeypadBox,
-  Button,
-} from "./styled";
-
-const Display = () => {
-  return (
-    <DisplayBox>
-      <DisplayInput />
-    </DisplayBox>
-  );
-};
-
-const Keypad = () => {
-  return (
-    <KeypadBox>
-      <Button>C</Button>
-      <Button>7</Button>
-      <Button>8</Button>
-      <Button>9</Button>
-      <Button>*</Button>
-      <Button>-</Button>
-      <Button>4</Button>
-      <Button>5</Button>
-      <Button>6</Button>
-      <Button>/</Button>
-      <Button>+</Button>
-      <Button>1</Button>
-      <Button>2</Button>
-      <Button>3</Button>
-      <Button>=</Button>
-      <Button>.</Button>
-      <Button>(</Button>
-      <Button>0</Button>
-      <Button>)</Button>
-      <Button>CE</Button>
-    </KeypadBox>
-  );
-};
-
-const ControlPanel = () => {
-  return <div>ControlPanel</div>;
-};
-
-const History = () => {
-  return <div>History</div>;
-};
+import Display from "Components/Display";
+import ControlPanel from "Components/ControlPanel";
+import Keypad from "Components/Keypad";
+import History from "../History";
+import { Container, CalculatorBlock, HistoryBlock } from "./styled";
 
 const Calculator = () => {
+  const [isVisible, setIsVisible] = React.useState(true);
+
   return (
     <Container>
       <CalculatorBlock>
+        <ControlPanel setIsVisible={setIsVisible} />
         <Display />
         <Keypad />
       </CalculatorBlock>
-      <HistoryBlock>
-        <ControlPanel />
-        <History />
-      </HistoryBlock>
+      {isVisible && (
+        <HistoryBlock>
+          <History />
+        </HistoryBlock>
+      )}
     </Container>
   );
 };
--- src/components/Calculator/styled.js
+++ src/components/Calculator/styled.js
@@ -2,66 +2,21 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: 4fr 1fr;
+  grid-template-columns: repeat(auto-fit, minmax(200px, auto));
   column-gap: 12px;
   padding: 10px 30px;
+
+  @media (max-width: 1299.98px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const CalculatorBlock = styled.div`
   padding: 0.5rem 1rem;
-  //   border: 10px dashed red;
+  position: relative;
 `;
 
 export const HistoryBlock = styled.div`
-  border: 10px dashed blue;
-`;
-
-// Display
-
-export const DisplayBox = styled.div``;
-
-export const DisplayInput = styled.input`
-  width: 100%;
-  padding: 15px 80px;
-  text-align: end;
-  font-size: 40px;
-  border: none;
-  background-color: ${(props) => props.theme.background};
-  color: ${(props) => props.theme.text};
-  border-bottom: 2px solid ${(props) => props.theme.board};
-
-  &:focus,
-  &:active {
-    outline: none;
-  }
-`;
-
-// Keypad
-
-export const KeypadBox = styled.div`
-  padding: 20px 80px;
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  column-gap: 150px;
-  row-gap: 40px;
-`;
-
-export const Button = styled.button`
-  width: 120px;
-  height: 120px;
-  font-size: 42px;
-  font-weight: 500;
-  border-radius: 20px;
-  background-color: ${(props) => props.theme.btnBg};
-  border: 2px solid ${(props) => props.theme.board};
-  color: ${(props) => props.theme.text};
-  transition: opacity 0.2s;
-
-  &:hover {
-    opacity: 0.7;
-  }
-
-  &:active {
-    box-shadow: 2px 2px 1px ${(props) => props.theme.accent};
-  }
+  border-left: 2px solid ${(props) => props.theme.board};
+  padding: 20px 30px;
 `;
